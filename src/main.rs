091a5e1f18876b52e2be929c7d09fbd48fcaaa29
AriArch <rogerfiblasalvado@gmail.com>
2023-06-20 11:39:04 +0200

    Add table output

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,16 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+use comfy_table::{
+    presets::UTF8_FULL,
+    modifiers::UTF8_ROUND_CORNERS,
+    Attribute,
+    Cell,
+    ContentArrangement,
+    Table,
+    TableComponent
+};
+
 const DEFAULT_FALIURE: &str = "Unknown";
 
 #[derive(Parser, Debug)]
@@ -79,19 +89,79 @@ fn main() {
         Err(_) => String::from(DEFAULT_FALIURE),
     };
 
-    println!("[Flyser]\n");
-    println!("File name: {}", args.file_name.split('/').last().unwrap_or(DEFAULT_FALIURE).split('.').next().unwrap_or(DEFAULT_FALIURE));
-    println!("File type: {file_type}");
-    println!(
-        "File path: {}",
-        file_path.to_str().unwrap_or(DEFAULT_FALIURE)
-    );
 
-    println!("\nFile size: {file_size:.5} MB");
+    let name = args.file_name.split('/').last().unwrap_or(DEFAULT_FALIURE).split('.').next().unwrap_or(DEFAULT_FALIURE);
 
-    println!("\nTotal number of lines: {line_count}");
-    println!("Total number or words: {word_count}");
-    println!("Total number of characters: {char_count}");
+    let f_size: f64 = round_num(file_size, 5);
+
+    let mut table = Table::new();
+
+    table.load_preset(UTF8_FULL)
+        .apply_modifier(UTF8_ROUND_CORNERS)
+        .set_content_arrangement(ContentArrangement::Dynamic)
+        .set_width(80)
+        .set_header(vec![
+                    String::from("Flyser <3")
+        ])
+        .add_row(vec![
+                 Cell::new("File name").add_attribute(Attribute::Bold).fg(comfy_table::Color::Red),
+                 Cell::new("\u{ea9f}").fg(comfy_table::Color::Red),
+                 Cell::new(&name)
+        ])
+        .add_row(vec![
+                 Cell::new("File type").add_attribute(Attribute::Bold).fg(comfy_table::Color::Yellow),
+                 Cell::new("\u{ea9f}").fg(comfy_table::Color::Yellow),
+                 Cell::new(&file_type)
+        ])
+        .add_row(vec![
+                Cell::new("File path").add_attribute(Attribute::Bold).fg(comfy_table::Color::Green),
+                Cell::new("\u{ea9f}").fg(comfy_table::Color::Green),
+                Cell::new(&file_path.to_str().unwrap_or(DEFAULT_FALIURE))
+        ])
+        .add_row(vec![
+                Cell::new("File size").add_attribute(Attribute::Bold).fg(comfy_table::Color::Cyan),
+                Cell::new("\u{ea9f}").fg(comfy_table::Color::Cyan),
+                Cell::new(format!("{} MB", &f_size))
+        ])
+        .add_row(vec![
+                 Cell::new("Lines").add_attribute(Attribute::Bold).fg(comfy_table::Color::Blue),
+                 Cell::new("\u{ea9f}").fg(comfy_table::Color::Blue),
+                 Cell::new(&line_count)
+        ])
+        .add_row(vec![
+                 Cell::new("Words").add_attribute(Attribute::Bold).fg(comfy_table::Color::Magenta),
+                 Cell::new("\u{ea9f}").fg(comfy_table::Color::Magenta),
+                 Cell::new(&word_count)
+        ])
+        .add_row(vec![
+                 Cell::new("Characters").add_attribute(Attribute::Bold).fg(comfy_table::Color::Red),
+                 Cell::new("\u{ea9f}").fg(comfy_table::Color::Red),
+                 Cell::new(&char_count)
+        ]);
+        
+    
+
+    let table = format_table(&mut table);
+    println!("{table}")
+}
+
+fn format_table(table: &mut Table) -> &mut Table {
+    table.set_style(TableComponent::VerticalLines, ' ');
+    table.remove_style(TableComponent::HorizontalLines);
+    table.remove_style(TableComponent::MiddleIntersections);
+    table.set_style(TableComponent::MiddleHeaderIntersections, '─');
+    table.remove_style(TableComponent::LeftBorderIntersections);
+    table.remove_style(TableComponent::RightBorderIntersections);
+    table.set_style(TableComponent::TopBorderIntersections, '─');
+    table.set_style(TableComponent::BottomBorderIntersections, '─');
+    table.set_style(TableComponent::HeaderLines, '─');
+    table.set_style(TableComponent::LeftHeaderIntersection, '├');
+    table.set_style(TableComponent::RightHeaderIntersection, '┤')
+}
+
+fn round_num(number: f64, decimal_places: u32) -> f64 {
+    let multiplier = 10_f64.powi(decimal_places as i32);
+    (number * multiplier).round() / multiplier
 }
 
 fn get_type_from_ext(ext: &str) -> &str {
